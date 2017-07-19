8fd04a427c9ae13942ac26f30248b5d101452257
Kathleen <kathleenmcclay@gmail.com>
2017-07-19 13:34:54 -0400

    Added header tags and working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,17 @@
 class PagesController < ApplicationController
 def welcome
+
 render :about
+
 end
 
 def about
+  @header = "This is the WELCOME page"
   render :about
+end
+
+def contest
+  @header = "This is the CONTEST page"
+  render :contest
 end
 end
